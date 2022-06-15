c3b71b61773d3fdedad5865fe6243636e1b819d9
Michael da Costa Mora <michael.c.mora@gmail.com>
2022-06-15 18:14:50 -0300

    Commit Inicial

---
+++ src/main/java/br/pucrs/spring/tstdemo/TstdemoApplication.java
@@ -0,0 +1,13 @@
+package br.pucrs.spring.tstdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TstdemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TstdemoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/pucrs/spring/tstdemo/TstdemoApplicationTests.java
@@ -0,0 +1,13 @@
+package br.pucrs.spring.tstdemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TstdemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
